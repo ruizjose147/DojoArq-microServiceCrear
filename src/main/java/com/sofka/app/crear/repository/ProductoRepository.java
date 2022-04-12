bf595bb42c8dae666144b7519e3f5a8ca19318ba
ruizjose147 <ruizjose_147@hotmail.com>
2022-04-12 12:35:16 -0500

    Correccion en el repository

--- src/main/java/com/sofka/app/crear/repository/ProductoRepository.java
+++ src/main/java/com/sofka/app/crear/repository/ProductoRepository.java
@@ -2,6 +2,8 @@ package com.sofka.app.crear.repository;
 
 import com.sofka.app.crear.model.Producto;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductoRepository extends ReactiveMongoRepository<Producto, String> {
 }
